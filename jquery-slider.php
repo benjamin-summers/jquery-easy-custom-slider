ff0ce266068408c159d1bcc520609e6b8d674e55
unknown <summerstyler2@gmail.com>
2018-09-05 02:43:31 -0400

    made slider work via shortcode

--- jquery-slider.php
+++ jquery-slider.php
@@ -78,11 +78,11 @@
         .jssorb051 .i.idn {opacity:.3;}
 
         /*jssor slider arrow skin 051 css*/
-        .jssora051 {display:block;position:absolute;cursor:pointer;}
-        .jssora051 .a {fill:none;stroke:#fff;stroke-width:360;stroke-miterlimit:10;}
-				.jssora051:hover {opacity:.8;}
-        .jssora051.jssora051dn {opacity:.5;}
-        .jssora051.jssora051ds {opacity:.3;pointer-events:none;}
+        .jssora094 {display:block;position:absolute;cursor:pointer;}
+        .jssora094 .a {fill:none;stroke:#fff;stroke-width:360;stroke-miterlimit:10;}
+				.jssora094:hover {opacity:.8;}
+        .jssora094.jssora094dn {opacity:.5;}
+        .jssora094.jssora094ds {opacity:.3;pointer-events:none;}
     </style>
     <div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:980px;height:380px;overflow:hidden;visibility:hidden;">
         <!-- Loading Screen -->
@@ -90,38 +90,23 @@
             <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="img/spin.svg" />
         </div>
         <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:980px;height:380px;overflow:hidden;">
-            <div>
-                <img data-u="image" src="img/001.jpg" />
+	        <div>
+		        <img src="<?php echo plugin_dir_url( __FILE__ ) . 'img/001.jpg'; ?>">"
+	        </div>
+	        <div>
+		        <img src="<?php echo plugin_dir_url( __FILE__ ) . 'img/001.jpg'; ?>">"
+	        </div>
+	        <div>
+		        <img src="<?php echo plugin_dir_url( __FILE__ ) . 'img/001.jpg'; ?>">"
             </div>
             <div>
-                <img data-u="image" src="img/002.jpg" />
+	            <img src="<?php echo plugin_dir_url( __FILE__ ) . 'img/001.jpg'; ?>">"
             </div>
             <div>
-                <img data-u="image" src="img/003.jpg" />
+	            <img src="<?php echo plugin_dir_url( __FILE__ ) . 'img/001.jpg'; ?>">"
             </div>
             <div>
-                <img data-u="image" src="img/004.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="img/005.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="img/006.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="img/007.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="img/009.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="img/010.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="img/015.jpg" />
-            </div>
-            <div>
-                <img data-u="image" src="img/022.jpg" />
+	            <img src="<?php echo plugin_dir_url( __FILE__ ) . 'img/001.jpg'; ?>">"
             </div>
         </div>
         <!-- Bullet Navigator -->
@@ -133,12 +118,12 @@
             </div>
         </div>
         <!-- Arrow Navigator -->
-        <div data-u="arrowleft" class="jssora051" style="width:65px;height:65px;top:0px;left:35px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
+        <div data-u="arrowleft" class="jssora094" style="width:65px;height:65px;top:0px;left:35px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
             <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
                 <polyline class="a" points="11040,1920 4960,8000 11040,14080 "></polyline>
             </svg>
         </div>
-        <div data-u="arrowright" class="jssora051" style="width:65px;height:65px;top:0px;right:35px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
+        <div data-u="arrowright" class="jssora094" style="width:65px;height:65px;top:0px;right:35px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
             <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
                 <polyline class="a" points="4960,1920 11040,8000 4960,14080 "></polyline>
             </svg>
@@ -148,3 +133,7 @@
 	<?php } ?>
 	<?php
 		add_shortcode('jquery-slider', 'jquery_scripts');
+		?>
+
+
+
